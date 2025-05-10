a4e1059bc6aed55bb8b98ea096b0c57eaa73f4f6
Miquel44 <91468404+Miquel44@users.noreply.github.com>
2025-05-10 17:44:46 +0200

    Primer Grafico interactivo Palestina

--- graficos_exploratorios_palestina.r
+++ graficos_exploratorios_palestina.r
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(plotly)
 #Histograma, muertes en el tiempo
 ggplot(casualties_daily, aes(x = report_date, y = ext_killed)) +
   geom_bar(stat = "identity", fill = "red") +labs(
@@ -9,3 +10,36 @@ ggplot(casualties_daily, aes(x = report_date, y = ext_killed)) +
     y = "Number of External Killed (ext_killed)"
   ) +
   theme_minimal() 
+
+
+#Histograma interactivo 2024
+
+Hist2024 <- ggplot(joined_data, aes(
+  x = date, 
+  y = -ext_killed,
+  text = paste(
+    "<b>Date:</b>", format(date, "%Y-%m-%d"), "<br>",
+    "<b>Killed:</b>", ext_killed, "<br>",
+    "<b>Context:</b> <a href='", url, "' target='_blank'>", content, "</a>"
+  )
+)) +
+  geom_bar(stat = "identity", fill = "red", color = "red") +
+  labs(
+    title = "Daily Killed Count Over Time (2024)",
+    x = "Report Date",
+    y = "Number of External Killed (ext_killed)"
+  ) +
+  theme_minimal() +
+  scale_y_continuous(labels = abs)
+
+# Personalizar el tooltip con CSS y estilo
+plot <- ggplotly(Hist2024, tooltip = "text") %>%
+  style(
+    hoverlabel = list(
+      bgcolor = "#333333",  # Fondo oscuro
+      font = list(color = "white", family = "Arial"),  # Texto blanco
+      bordercolor = "lightgrey"  # Borde gris claro
+    )
+  )
+
+plot
