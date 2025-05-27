cb84d08fb8b8e8440f601cc448fade61aae79a64
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2025-05-27 08:51:28 +0200

    Update run_container.sh

--- server/run_container.sh
+++ server/run_container.sh
@@ -3,5 +3,5 @@ echo "Execute delete."
 bash delete_container.sh
 
 echo "Run server container."
-container_id=$(docker run -d --name server -p 80:8080 server)
+container_id=$(docker run -d --name server -p 8080:8080 server)
 echo "Server container running. ID: $container_id"S
