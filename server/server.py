cf2e03bd9cb699620ee13b3bd5382030caa63dda
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2025-05-27 16:39:41 +0200

    test

--- server/server.py
+++ server/server.py
@@ -173,16 +173,16 @@ class Server(environment.http.server.BaseHTTPRequestHandler):
                     environment.PUBLIC_HTML_FILES_PATH,
                     'graphs',
                     'infrastructure_loss',
-                    'placeholder.html',
+                    'infrastructure_loss.html',
                 ),
                 'text/html',
             )
-        elif parsed_path == '/images/infrastructure_final.gif':
+        elif parsed_path == '/images/infrastructure_loss.gif':
             self._serve_file(
                 environment.os.path.join(
                     environment.PUBLIC_HTML_FILES_PATH,
                     'images',
-                    'infrastructure_final.gif',
+                    'infrastructure_loss.gif',
                 ),
                 'image/gif',
             )
