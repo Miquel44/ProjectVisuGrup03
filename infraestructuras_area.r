44f8edbef61d8793f5e43d30b8020d3fd726d528
aminasifar1 <1672430@uab.cat>
2025-05-28 00:06:46 +0200

    Update infraestructuras_area.r

--- infraestructuras_area.r
+++ infraestructuras_area.r
@@ -1,8 +1,75 @@
 #-----------------------ANIMATION---------------------#
+
 library(tidyverse)
 library(gganimate)
-library(gifski)
 library(stringr)
+
+# Directori i càrrega de dades
+setwd('C:\\Users\\minaa\\OneDrive\\Escritorio\\Documentos\\EDD\\3r any\\2n Semestre\\VD\\Projecte\\ProjectVisuGrup03-main\\data')
+data <- read_csv("infra.csv", na = c("NA"))
+
+# Conversió de dates
+data <- data %>% mutate(report_date = as.Date(report_date))
+
+# Neteja de NAs
+data <- data %>%
+  mutate(across(starts_with("civic_buildings.ext_") | 
+                  starts_with("educational_buildings.ext_") | 
+                  starts_with("places_of_worship.ext_"),
+                ~replace_na(., 0)))
+
+# Selecció i transformació a format long
+data_ext <- data %>%
+  select(report_date, 
+         starts_with("civic_buildings.ext_"), 
+         starts_with("educational_buildings.ext_"), 
+         starts_with("places_of_worship.ext_"))
+
+data_long <- data_ext %>%
+  pivot_longer(cols = -report_date, names_to = "type", values_to = "value") %>%
+  mutate(type = str_replace(type, "^([^.]+)\\.ext_(.*)$", "\\1_\\2"))
+
+# Traducció dels noms a anglès
+noms_amigables <- tibble(
+  type = c(
+    "civic_buildings_destroyed", 
+    "civic_buildings_damaged",
+    "educational_buildings_destroyed",
+    "educational_buildings_damaged",
+    "places_of_worship_mosques_destroyed", 
+    "places_of_worship_mosques_damaged", 
+    "places_of_worship_churches_destroyed"
+  ),
+  nom_amic = c(
+    "Destroyed civic buildings", 
+    "Damaged civic buildings",
+    "Destroyed educational buildings",
+    "Damaged educational buildings",
+    "Destroyed mosques", 
+    "Damaged mosques", 
+    "Destroyed churches"
+  )
+)
+
+# Unió amb noms i càlcul acumulat
+data_long <- data_long %>%
+  left_join(noms_amigables, by = "type") %>%
+  mutate(nom_amic = if_else(is.na(nom_amic), type, nom_amic)) %>%
+  arrange(nom_amic, report_date) %>%
+  group_by(nom_amic) %>%
+  mutate(value_acum = cumsum(value)) %>%
+  ungroup()
+
+# Ordre personalitzat (per a l'stacking)
+ordre_factors <- c(
+  "Destroyed mosques", 
+  "Damaged mosques",
+  "Destroyed civic buildings", 
+  "Damaged civic buildings",
+  "Destroyed educational buildings",
+  "Damaged educational buildings",
+  "Destroyed churches"
+)
 
 colors_palestina <- c(
   "Destroyed mosques" = "#007A3D",
